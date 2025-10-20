d7989bfffd790bac028f9c54e617f8991f11191b
Daniel Yim <jedidiahcelloboy@hotmail.com>
2025-10-20 13:27:48 -0400

    add readme

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -15,9 +15,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <ColorSchemeScript />
-        <link rel="icon" type="image/png" href="/bg-fire.png" />
-        <link rel="shortcut icon" type="image/png" href="/bg-fire.png" />
-        <link rel="apple-touch-icon" href="/bg-fire.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico?v=2" />
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico?v=2" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link 
